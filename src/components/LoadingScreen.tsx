2ba4cfe7c6a010e21dfbe18e0d5f79187a3b5991
James Leo <ocampojamesleo04@gmail.com>
2024-10-08 21:12:04 +0800

    added spinner icon

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,15 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { Spinner } from "@phosphor-icons/react";
 
 const LoadingScreen = () => {
-	return <>Loading...</>;
+	return (
+		<>
+			<Box width={"100%"} height={"100%"}>
+				<Spinner />
+			</Box>
+		</>
+	);
 };
 
 export default LoadingScreen;
