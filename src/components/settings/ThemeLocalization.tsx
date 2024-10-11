88db08569035dcbab0a0085112f5cc6c150d7b59
James Leo <136047998+JamesWithLeo@users.noreply.github.com>
2024-10-11 12:38:37 +0800

    Update ThemeLocalization.tsx

--- src/components/settings/ThemeLocalization.tsx
+++ src/components/settings/ThemeLocalization.tsx
@@ -1,41 +1,25 @@
+import PropTypes from "prop-types";
 // @mui
-import { ThemeProvider, createTheme, ThemeOptions } from "@mui/material/styles";
-import useSettings from "../../hooks/useSettings";
+import { ThemeProvider, createTheme, useTheme } from "@mui/material/styles";
 // hooks
-import palette from "../../theme/palette";
-import { customShadows } from "../../theme/shadows";
-import typography from "../../theme/typography";
-import breakpoints from "../../theme/breakpoints";
-import { shadows } from "../../theme/shadows";
-
 import useLocales from "../../hooks/useLocales";
-import { ReactNode, useMemo } from "react";
+import { ReactNode } from "react";
+
+// ----------------------------------------------------------------------
+
+ThemeLocalization.propTypes = {
+	children: PropTypes.node.isRequired,
+};
 
 export default function ThemeLocalization({
 	children,
 }: {
 	children: ReactNode;
 }) {
-	const { themeMode, themeDirection } = useSettings();
-
-	const isLight = themeMode === "light";
-
-	const themeOptions: ThemeOptions = useMemo(
-		() => ({
-			palette: isLight ? palette.light : palette.dark,
-			typography,
-			breakpoints,
-			shape: { borderRadius: 8 },
-			direction: themeDirection,
-			shadows: isLight ? shadows.light : shadows.dark,
-			customShadows: isLight ? customShadows.light : customShadows.dark,
-		}),
-		[isLight, themeDirection],
-	);
-
+	const defaultTheme = useTheme();
 	const { currentLang } = useLocales();
 
-	const theme = createTheme(themeOptions, currentLang.systemValue);
+	const theme = createTheme(defaultTheme, currentLang.systemValue);
 
 	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
