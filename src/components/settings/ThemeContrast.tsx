00f28050ce5e052a9ab273011e2ca9f957b7d473
James Leo <136047998+JamesWithLeo@users.noreply.github.com>
2024-10-11 12:38:09 +0800

    Update ThemeContrast.tsx

--- src/components/settings/ThemeContrast.tsx
+++ src/components/settings/ThemeContrast.tsx
@@ -1,120 +1,50 @@
-import { ReactNode, useMemo } from "react";
+import { useMemo } from "react";
 // @mui
-import { CssBaseline } from "@mui/material";
 import {
 	alpha,
 	ThemeProvider,
 	createTheme,
 	useTheme,
-	ThemeOptions,
 } from "@mui/material/styles";
 // hooks
 import useSettings from "../../hooks/useSettings";
 //
 import componentsOverride from "../../theme/overrides";
-import palette from "../../theme/palette";
+import { ThemeOptions } from "@mui/material/styles/createTheme";
 import { customShadows } from "../../theme/shadows";
 
-export default function ThemeContrast({ children }: { children: ReactNode }) {
+export default function ThemeColorPresets({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	const defaultTheme = useTheme();
 
-	const { themeContrast, themeMode } = useSettings();
-
-	const isLight = defaultTheme.palette.mode === "light";
-
-	const shadowColor = isLight
-		? defaultTheme.palette.grey[500]
-		: defaultTheme.palette.common.black;
-
-	const styles = {
-		bgDefault: defaultTheme.palette.background.default,
-		bgBold: isLight
-			? defaultTheme.palette.grey[100]
-			: defaultTheme.palette.grey[900],
-		cardDefault: defaultTheme.components?.MuiCard?.styleOverrides?.root,
-		cardBold: {
-			zIndex: 0,
-			position: "relative",
-			borderRadius: Number(defaultTheme.shape.borderRadius) * 2,
-			boxShadow: `0 0 1px 0 ${alpha(shadowColor, 0.48)}, 0 2px 4px -1px ${alpha(shadowColor, 0.24)}`,
-		},
-	};
+	const { setColor, themeMode } = useSettings();
 	const isDarkMode = themeMode === "dark";
-
 	const themeOptions: ThemeOptions = useMemo(
 		() => ({
 			...defaultTheme,
-			palette: isDarkMode
+			palette: {
+				...defaultTheme.palette,
+				primary: setColor,
+			},
+			customShadows: isDarkMode
 				? {
-						...palette.dark,
-						background: {
-							...defaultTheme.palette.background,
-							default:
-								themeContrast === "bold"
-									? styles.bgBold
-									: styles.bgDefault,
-						},
+						...customShadows.dark,
+						primary: `0 8px 16px 0 ${alpha(setColor.main, 0.24)}`,
 					}
 				: {
-						...palette.light,
-						background: {
-							...defaultTheme.palette.background,
-							default:
-								themeContrast === "bold"
-									? styles.bgBold
-									: styles.bgDefault,
-						},
+						...customShadows.light,
+						primary: `0 8px 16px 0 ${alpha(setColor.main, 0.24)}`,
 					},
-			customShadows: isDarkMode
-				? customShadows.dark
-				: customShadows.light,
-
-			components: {
-				MuiCard: {
-					styleOverrides: {
-						root: {
-							zIndex: 1,
-							position: "relative",
-							borderRadius: 12,
-							boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
-
-							variants: [
-								{
-									props: { raised: true },
-									style: {
-										boxShadow:
-											"0px 6px 30px rgba(0, 0, 0, 0.2)",
-									},
-								},
-							],
-						},
-					},
-				},
-			},
 		}),
-
-		[
-			defaultTheme,
-			themeContrast,
-			isDarkMode,
-			styles.bgBold,
-			styles.bgDefault,
-			// styles.cardBold,
-			// styles.cardDefault,
-		],
+		[setColor, defaultTheme, isDarkMode],
 	);
 
 	const theme = createTheme(themeOptions);
 
-	theme.components = {
-		...componentsOverride(theme),
-		MuiCard: themeOptions.components?.MuiCard,
-	};
+	theme.components = componentsOverride(theme);
 
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			{children}
-		</ThemeProvider>
-	);
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
