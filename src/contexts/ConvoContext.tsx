7541c71875a910275a528a462579871cc328610a
James Leo <ocampojamesleo04@gmail.com>
2024-12-17 21:25:12 +0800

    status indicator v0.3

--- src/contexts/ConvoContext.tsx
+++ src/contexts/ConvoContext.tsx
@@ -121,6 +121,12 @@ const ConvoContextProvider: React.FC<ConvoContextProviderProps> = ({
 				},
 			);
 		});
+		return () => {
+			if (id) {
+				socket.emit("peersStatus", { sender_id: id, isOnline: false });
+				socket.disconnect();
+			}
+		};
 	}, [queryClient, conversation, isSuccess]);
 	return (
 		<ConvoContext.Provider
